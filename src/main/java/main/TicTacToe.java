cb9773dbc031425f7c5cd9ce94925a2814e8aba7
dell <dell@dell-PC>
2019-04-28 21:04:58 +0530

    tictactoe

--- src/main/java/main/TicTacToe.java
+++ src/main/java/main/TicTacToe.java
@@ -5,21 +5,84 @@ import java.util.*;
 public class TicTacToe{
 public static int[][] ticbox;
 	public static int win(int [][]tic) {
-		//check all rows
-		//loop through rows from 0 to 3 and check if all the 3 places have same marks
- 		
 		
+	//check all rows
+		//loop through rows from 0 to 3 and check if all the 3 places have same mark
+		int count = 0,count1 = 0;
+		for(int i=0; i<3; i++) {
+			count = 0; count1 = 0;
+			for(int j = 0; j<3; j++) {
+				if(tic[i][j]==1)
+				{
+					count++;
+					if(count == 3)
+					{
+						return 1;
+					}
+				}
+				if(tic[i][j]==2)
+				{
+					count1++;
+					{
+						if(count1 == 3)
+						{
+							return 2;
+						}
+					}
+				}
+			}
+		}
 		//check all cols
-		//loop through columns from 0 to 3 and check if all the 3 places have same marks
- 		
+		//loop through columns from 0 to 3 and check if all the 3 places have same marks	
+		int count2 = 0,count3 = 0;
+		for(int i=0; i<3; i++) {
+			count2 = 0; count3 = 0;
+			for(int j = 0; j<3; j++) {
+				if(tic[j][i]==1)
+				{
+					count2++;
+					if(count2 == 3)
+					{
+						return 1;
+					}
+				}
+				if(tic[j][i]==2)
+				{
+					count3++;
+					{
+						if(count3 == 3)
+						{
+							return 2;
+						}
+					}
+				}
+			}
+		}
 		//check both diagonals 
-		
-		
-		
+		int count4 = 0;int count5 = 0;
+		for(int i = 0; i<3 ; i++)
+		{
+			for(int j = i; j<i ;j++)
+			{
+				if(tic[i][j]==1)
+				{
+					count4++;
+					if(count4 == 3)
+					{
+						return 1;
+					}
+				}
+				if(tic[i][j]==2) 
+				{
+					count5++;
+					if(count5 == 3)
+					{
+						return 2;
+					}
+				}
+			}
+		}
 		//write your code here !!!
-		
-		
-		
 		return 0;
 		
 	}
@@ -37,12 +100,14 @@ public static int[][] ticbox;
 		//check if a2 is between 0 & 3
 		//check if the selected box is empty ie, already not marked by other player
 		//if all checks passed return true.
-		
-		
-		//Write your code here !!!
-		
-		
+		if((a1>=0 && a1<3)&&(a2>=0 && a2<3) && (tic[a1][a2]== 0))
+		{
+		return true;
+		}
+		else
+		{
 		return false;
+		}
 	}
 	public static void main(String args[]) {
 		
@@ -75,7 +140,8 @@ public static int[][] ticbox;
 				else {
 					System.out.println("Please enter a valid position!!");
 				}
-			}else {
+			}
+			else {
 				System.out.println("Player 2 Enter the box number");
 				printBox(ticbox);
 				Scanner sc = new Scanner(System.in);
